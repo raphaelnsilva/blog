7e775ff62ab74a898b810af542cb318e9eb3e982
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-08-16 13:29:40 -0300

    create new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,63 @@
 import './App.css';
-
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from "firebase/auth";
 
-//pages 
+// hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAutentication';
+
+// CONTEXT
+import { AuthProvider } from './context/AuthContext';
+
+// PAGES
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
 
-//Components
+// COMPONENTS
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
 
+  // VALIDAÇÃO DE USUARIO LOGADO.
+  const [ user, setUser ] = useState(undefined);
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  // usa o efeito de usuário validado
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if(loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/post/create' element={<CreatePost />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,40 @@
 import { NavLink } from 'react-router-dom';
+
+import {useAuthentication} from '../hooks/useAutentication';
+import {useAuthValue} from '../context/AuthContext';
+
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+
+  const {user} = useAuthValue();
+
   return (
     <>
       <nav className={styles.navbar}>
-
         <NavLink to="/" className={styles.brand}>
           <span>BLOG</span>
         </NavLink>
-
         <ul className={styles.links_list}>
           <li>
             <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>
               Home
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>
-              Entrar
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>
-              Cadastrar
-            </NavLink>
-          </li>
+          {!user && (
+            <>
+              <li>
+                <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>
+                  Entrar
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>
+                  Cadastrar
+                </NavLink>
+              </li>
+            </>
+          )}
           <li>
             <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>
               Sobre
